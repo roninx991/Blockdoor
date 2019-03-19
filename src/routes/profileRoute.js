4b856370ece5d89561f6380e75fc3fcaa4122cea
Sangat <sangatdas5@gmail.com>
2019-03-19 10:45:14 +0530

    Updates

--- src/routes/profileRoute.js
+++ src/routes/profileRoute.js
@@ -22,36 +22,26 @@ var p_router = function() {
             if (!req.user) {
                 res.redirect('/');
             } else {
-<<<<<<< HEAD
-                res.render('profile', {
-                    title: "SmartReviewer",
-                    navMenu: menu,
-                    user: req.user,
-                    balance: 0
-                });
-=======
                 const url = 'mongodb://localhost:27017';
                 mongodb.connect(url, { useNewUrlParser: true }, function(err, client) {
                     const db = client.db('NodeDemoWebApp');
                     const Submissions = db.collection('Submissions');
 
-                    Submissions.find({owner:req.user._id}).toArray(function(err,ans){
-                        if(err){
-                            console.log(err);    
-                        }
-                        else{
+                    Submissions.find({ owner: req.user._id }).toArray(function(err, ans) {
+                        if (err) {
+                            console.log(err);
+                        } else {
                             res.render('profile', {
                                 title: "SmartReviewer",
                                 navMenu: menu,
-                                user: req.user, 
-                                sub : ans
+                                user: req.user,
+                                sub: ans
                             });
-                             console.log(ans);
+                            console.log(ans);
 
-                        }    
-                    });    
-                });          
->>>>>>> 94b6b587b57a998f8fdc422f470ec7edf34054a7
+                        }
+                    });
+                });
             }
         });
 
